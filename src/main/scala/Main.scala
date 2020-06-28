4ba56d0c667cc532ef4141c0894c6c00a6254042
kotLeon <katmas2@st.amu.edu.pl>
2020-06-28 17:00:35 +0200

    adding two functions

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -20,11 +20,24 @@ trait BinaryTree {
       if (help(this, 0) < 0) Node(left, right.addLeaf(leaf))
       else Node(left.addLeaf(leaf), right)
   }
+  
+  // multiplies all elements of the tree
+  def multiplyElementsOfTree() : Int = {
+      def recMult(tree: BinaryTree, acc: Int) : Int = tree match {
+          case Leaf(value) => acc * value
+          case Node(left, right) => recMult(left, acc) * recMult(right, acc)
+      }
+      recMult(this, 1)
+  }
+  
   def help(tree: BinaryTree, acc: Int): Int = tree match{
       case Leaf(_) => acc + 1
       case Node(left, right) => help(left, acc) - help(right, acc)
     }
-
+    
+  // creates new Tree, which consists of two trees
+  def cloneTree() : BinaryTree = Node(this, this)
+  
 }
 
 case class Leaf(value: Int) extends BinaryTree
@@ -37,4 +50,10 @@ object Main extends App {
   println(tree.sumTree())
   println(tree.addLeaf(Leaf(4)))
 
+  println("multiplication testing!")
+  val secTree = Node(Node(Leaf(1), Leaf(2)), Node(Leaf(3), Leaf(4)))
+  println(secTree.multiplyElementsOfTree())
+  val clonedTree = secTree.cloneTree()
+  println(clonedTree.multiplyElementsOfTree())
+  println(clonedTree)
 }
